3bbbb2e57cae609acd3f4640f638b4c241473d18
yoshitaka motomura <stay.daybreak@gmail.com>
2020-09-03 01:15:09 +0900

    imploment server

---
+++ src/auth.ts
@@ -0,0 +1,35 @@
+export type UserInterface = {
+  [key: string]: string | number | boolean | undefined
+  id: number
+  email: string
+  password?: string
+  login: boolean
+}
+export const Users: UserInterface[] = [
+  {
+    id: 1,
+    email: 'test@example.com',
+    password: 'password',
+    login: true,
+  },
+  {
+    id: 2,
+    email: 'hello@example.com',
+    password: 'password',
+    login: true,
+  },
+]
+export function authenticate(email: string, password: string): UserInterface {
+  const user: UserInterface | undefined = Users.find((v) => {
+    return v.email === email && v.password === password
+  })
+  if (!user) {
+    return {
+      id: 0,
+      email: '',
+      login: false,
+    }
+  }
+  delete user.password
+  return user
+}
--- tests/Message.test.ts
+++
@@ -1,8 +0,0 @@
-import Message from '../src/Message'
-
-describe('Message Class Test', () => {
-  it('out hello world', () => {
-    const msg = new Message('hello')
-    expect(msg.out()).toBe('hello world')
-  })
-})
